1d9154915d5ce2df29a08693df4669a18b97868f
Nirmal Jain <jainnirmal1992@gmail.com>
2023-01-12 17:36:55 +0530

    refactored code

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req, res) => { 
+  res.type(".html")
+  res.status(200).end('Hello World!')
 })
 
 app.listen(port, () => {
