3ff36d19e147977411fbe24dab4adab28de8051c
Nirmal Jain <jainnirmal1992@gmail.com>
2023-01-12 17:50:36 +0530

    refactored code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 3000
 
 app.get('/', (req, res) => { 
   res.type(".html")
-  res.status(200).end('Hello World!')
+  res.status(200).end('Hello World Nirmal!')
 })
 
 app.listen(port, () => {
